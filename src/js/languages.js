c53785731afeafd0cf32702f483897b7c23ab18a
Olexander96 <o.sheiko96@gmail.com>
2022-11-05 09:48:27 +0200

    completed quality statistics and what-is sections

--- src/js/languages.js
+++ src/js/languages.js
@@ -59,4 +59,113 @@ const langArr = {
         'ua': 'Ми пропонуємо рішення, яке "очищає" трафік та підвищує його якість. Рекламна площадка Диплекс.в мережі розроблено власний цифровий алгоритм розміщення, за технологією блокчейн, що не має аналогів на ринку. За допомогою нашої технології рекламодавці взаємодіють з активною та зацікавленою аудиторією.',
         'ru': 'Мы предлагаем решение, которое "очищает" трафик и повышает его качество. Рекламная площадка Диплекс.в сети разработан собственный цифровой алгоритм размещения, по технологии блокчейн, который не имеет аналогов на рынке. С помощью нашей технологии рекламодатели взаимодействуют с активной и заинтересованной аудиторией.',
     },
+    // statistics section
+    'statistics-title': {
+        'en': 'Statistics',
+        'ua': 'Статистика',
+        'ru': 'Статистика',
+    },
+    'statistics-card1': {
+        'en': 'Wallets loaded',
+        'ua': 'Завантажено гаманців',
+        'ru': 'Скачано кошельков',
+    },
+    'statistics-card2': {
+        'en': 'members',
+        'ua': 'Учасників',
+        'ru': 'Участников',
+    },
+    'statistics-card3': {
+        'en': 'Coins issued',
+        'ua': 'Випущено монет',
+        'ru': 'Выпущено монет',
+    },
+    'statistics-card4-title': {
+        'en': '1% per day',
+        'ua': '1% за добу',
+        'ru': '1% в сутки',
+    },
+    'statistics-card4': {
+        'en': 'Earnings',
+        'ua': 'Заробіток',
+        'ru': 'Заработок',
+    },
+    'statistics-card5': {
+        'en': 'Growth per day',
+        'ua': 'Ріст за добу',
+        'ru': 'Рост в сутки',
+    },
+    // what-is section
+    'what-is-title': {
+        'en': 'What is DLX?',
+        'ua': 'Що таке DLX?',
+        'ru': 'Что такое DLX?',
+    },
+    'what-is-info1': {
+        'en': 'Every year the wheel of progress turns even faster, investing has never been easier thanks to digital currencies and blockchain technology. We are active participants in the IT market, in connection with this we issue shares of the company in the digital format of the cryptocurrency Diplex Coin DLX.',
+        'ua': 'З кожним роком колесо прогресу обертається ще швидше, інвестування ніколи не було простіше і завдяки цифровим валютам і технології блокчейн. Ми є активними учасниками ІТ-ринку, тому випускаємо акції компанії в цифровому форматі криптовалюти-Diplex Coin DLX.',
+        'ru': 'С каждым годом колесо прогресса вращается еще быстрее, инвестирование никогда не было проще и все благодаря цифровым валютам и технологии блокчейн. Мы являемся активными участниками ИТ-рынка, в связи с этим выпускаем акции компании в цифровом формате криптовалюты-Diplex Coin  DLX.',
+    },
+    'what-is-info2': {
+        'en': 'By purchasing shares of our platform, you not only help us become better, but also get full access to the services provided by our services. DLX is a digital currency, a kind of "fuel" for the operation of our services, especially the Diplex advertising platform. On the web. All payments made in the Diplex group ecosystem are made using our own cryptocurrency - advertisers use DLX to pay for service services, and users receive DLX to perform various tasks.Okay, right?',
+        'ua': 'Купуючи акції нашої платформи, ви не тільки допомагаєте нам стати кращими, але й отримуєте повний доступ до послуг, що надаються нашими сервісами. DLX-це цифрова валюта, своєрідне "паливо" для роботи наших сервісів, особливо рекламної платформи Diplex. У мережі. Всі розрахунки, що проводяться в екосистемі Diplex group, проводяться з використанням власної криптовалюти-рекламодавці використовують DLX для оплати послуг сервісу, а користувачі отримують DLX для виконання різних завдань Добре, правда?',
+        'ru': 'Приобретая акции нашей платформы, вы не только помогаете нам стать лучше, но и получаете полный доступ к услугам, предоставляемым нашими сервисами. DLX-это цифровая валюта, своеобразное "топливо” для работы наших сервисов, особенно рекламной платформы Diplex.В сети. Все расчеты, проводимые в экосистеме Diplex group, производятся с использованием собственной криптовалюты-рекламодатели используют DLX для оплаты услуг сервиса, а пользователи получают DLX для выполнения различных задач. Хорошо, правда?',
+    },
+    // benefits section
+    'benefits-title': {
+        'en': 'Benefits of owning a DLX',
+        'ua': 'Переваги володіння DLX',
+        'ru': 'Преимущества владения DLX',
+    },
+    'benefits-list__item1-titile': {
+        'en': 'Security and control',
+        'ua': 'Безпека та контроль',
+        'ru': 'Безопастность и контроль',
+    },
+    'benefits-list__item1-info': {
+        'en': 'Your funds are protected by the DIPLEX cryptocurrency on the blockchain.',
+        'ua': 'Ваші засоби захищені криптовалютою DIPLEX на блокчейні.',
+        'ru': 'Ваши средства защищены криптовалютой DIPLEX на блокчейне.',
+    },
+    'benefits-list__item2-titile': {
+        'en': 'Works anytime, anywhere',
+        'ua': 'Працює завжди і скрізь',
+        'ru': 'Работает всегда и везде',
+    },
+    'benefits-list__item2-info': {
+        'en': 'DLX always works, even on holidays! 24/7 support.',
+        'ua': 'DLX працює завжди, навіть у свята! Цілодобова підтримка.',
+        'ru': 'DLX работает всегда, даже в праздники! Круглосуточная поддержка.',
+    },
+    'benefits-list__item3-titile': {
+        'en': 'International payments',
+        'ua': 'Міжнародні платежі',
+        'ru': 'Международные платежи',
+    },
+    'benefits-list__item3-info': {
+        'en': '1000 transactions per second, no fees, unlimited transfers.',
+        'ua': '1000 транзакцій на секунду, відсутність комісій, перекази без обмежень.',
+        'ru': '1000 транзакций в секунду, отсутствие комиссий, переводы без ограничений.',
+    },
+    // quality section
+    'quality-servicse__title': {
+        'en': 'Get high quality services using the Diplex Group service.',
+        'ua': 'Отримайте високу якість послуг за допомогою Diplex Group.',
+        'ru': 'Получите высокое качество услуг, используя сервис Diplex Group.',
+    },
+    'quality-servicse__info': {
+        'en': 'Earn money by supporting our blockchain network. Store DLX on a dedicated wallet and get rewarded - earned coins can be minted (PoS), thus increasing the amount of stored DLX up to 1% per day thanks to a given algorithm!',
+        'ua': 'Заробляйте гроші, підтримуючи нашу блокчейн-мережу. Зберігайте DLX на виділеному гаманці і отримуйте винагороду-зароблені монети можна "чеканити" (PoS), забезпечуючи таким чином збільшення обсягу DLX, що зберігаються, до 1% на добу завдяки заданому алгоритму!',
+        'ru': 'Зарабатывайте деньги, поддерживая нашу блокчейн-сеть. Храните DLX на выделенном кошельке и получайте вознаграждение-заработанные монеты можно "чеканить" (PoS), обеспечивая таким образом увеличение объема хранимых DLX до 1% в сутки благодаря заданному алгоритму!',
+    },
+    'quality-pos__title': {
+        'en': 'What is Proof-of-Stake (PoS)?',
+        'ua': 'Що таке Proof-of-Stake (PoS)?',
+        'ru': 'Что такое Proof-of-Stake (PoS)?',
+    },
+    'quality-pos__info': {
+        'en': 'This is one of the types of mining (PoS - proof of share), which uses an algorithm for distributing coins in the network. In the process of mining (chasing people) you get a reward in the form of additional coins. The idea of proof-of-stake is to solve the proof-of-work problem associated with high energy costs and financial investments.',
+        'ua': 'Це один із видів майнінгу (PoS – підтвердження частки), в якому використовується алгоритм розподілу монет у мережі. У процесі майнінгу (у гонитві за людьми) ви отримуєте винагороду у вигляді додаткових монет. Ідея proof-of-stake полягає у вирішенні проблеми proof-of-work, пов\'язаної з великими енергетичними витратами та фінансовими інвестиціями.',
+        'ru': 'Это один из видов майнинга (PoS - подтверждение доли), в котором используется алгоритм распределения монет в сети. В процессе майнинга (в погоне за людьми) вы получаете вознаграждение в виде дополнительных монет. Идея proof-of-stake заключается в решении проблемы proof-of-work, связанной с большими энергетическими затратами и финансовыми инвестициями.',
+    },
 }
